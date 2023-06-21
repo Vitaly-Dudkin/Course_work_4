08c394a5b6f8f44cd63e32d2fd4cc6fe4f9be96c
vitaly <dudkin231@mail.ru>
2023-06-21 10:57:57 +0300

    added abstract_db.py, abstract_parser.py, colors.py, config.py, json_exceptions_handling.py

---
+++ VacanciesAPIScrapping/VacanciesApiParser/Project/additional_classes/abstract_db.py
@@ -0,0 +1,36 @@
+"""
+ Abstract class for any db
+ New db realisation should implement 3 methods:
+
+ 1. get_vacancies (get list of vacancies from the db)
+ 2. save_vacancies (save list of vacancies to the db)
+ 3. delete_vacancies (delete list of vacancies from the db)
+
+"""
+from abc import ABC, abstractmethod
+
+from .vacancy_impl.vacancy import Vacancy
+
+
+class GenericDB(ABC):
+
+    @staticmethod
+    @abstractmethod
+    def get_vacancies() -> list[Vacancy]:
+        """
+        :return: List of the class: 'Vacancy' objects containing information about vacancies
+        """
+        pass
+
+    @staticmethod
+    @abstractmethod
+    def save_vacancies(vacancies: list[Vacancy]) -> None:
+        """
+        :param vacancies: List of the class: 'Vacancy' objects containing information about vacancies
+        """
+        pass
+
+    @staticmethod
+    @abstractmethod
+    def delete_vacancies() -> None:
+        pass
---
+++ VacanciesAPIScrapping/VacanciesApiParser/Project/utils/colors.py
@@ -0,0 +1,14 @@
+"""
+Class for the color syntax
+
+"""
+
+from colorama import Fore
+
+
+class Color:
+    white = Fore.LIGHTWHITE_EX
+    cyan = Fore.LIGHTCYAN_EX
+    yellow = Fore.LIGHTYELLOW_EX
+    red = Fore.LIGHTRED_EX
+    green = Fore.LIGHTGREEN_EX
